70bf7f03e7d2cd52c6090419a5dfa4c9c3ee7a7a
Vislan <s144510@ap.be>
2025-01-06 16:50:30 +0100

    folder organisation characters

---
+++ Characters/Character.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GameDevelopmentProject.Characters.Hero;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameDevelopmentProject.Characters
+{
+    public abstract class Character : IGameObject
+    {
+        public enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            Idle
+        }
+        protected Texture2D texture;
+        protected Animation.Animation animation;
+        public Vector2 position;
+        public Direction currDirection;
+        public int idleFrameIndex;
+        public int frameWidth;
+        public int frameHeight;
+        public Vector2 currSpeed;
+        public Vector2 walkSpeed;
+        public Vector2 maxSpeed;
+        public Vector2 accSpeed;
+
+        public Character(Texture2D texture, int fwidth, int fheight, Direction startDirection)
+        {
+            this.texture = texture;
+            frameWidth = fwidth;
+            frameHeight = fheight;
+            currDirection = startDirection;
+            position = new Vector2(Game1.screenWidth / 2 - fwidth / 2, Game1.screenHeight / 2 - fheight / 2);
+            animation = new Animation.Animation();
+            MakeAnimation(animation, fwidth, fheight);
+        }
+
+        protected void MakeAnimation(Animation.Animation anim, int fwidth, int fheight)
+        {
+            int rows = 4; // rijen zijn de 4 richtingen
+            int columns = 4; // per richting 4 frames de kolomen
+
+            for (int row = 0; row < rows; row++) // voor elke richting...
+            {
+                for (int col = 0; col < columns; col++) // ..elke frame toevoegen
+                {
+                    int x = col * fwidth; // x en y stellen de positie voor van waar de frame start
+                    int y = row * fheight; // dus 1 frame is richting/frame maal dimensies ve frame
+
+                    animation.AddFrame(new Animation.AnimationFrame(new Rectangle(x, y, fwidth, fheight)));
+                }
+            }
+        }
+
+        public abstract void Move(GameTime gameTime, KeyboardState input);
+        public virtual void Update(GameTime gameTime)
+        {
+            animation.Update(gameTime);
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, animation.currFrame.sourceRectangle, Color.White);
+
+        }
+    }
+}
---
+++ Characters/Hero.cs
@@ -0,0 +1,177 @@
+﻿using GameDevelopmentProject.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace GameDevelopmentProject.Characters
+{
+    public class Hero : Character
+    {
+        public int lives = 3;
+        private CollisionManager collisionManager = new CollisionManager();
+        public List<Civilian> civs = new List<Civilian>();
+        public Hero(Texture2D texture, int fwidth, int fheight, Direction startDirection) : base(texture, fwidth, fheight, startDirection)
+        {
+            maxSpeed = new Vector2(6, 6);
+            accSpeed = new Vector2(0.1f, 0.1f);
+            currSpeed = new Vector2(0, 0);
+        }
+
+        public override void Move(GameTime gameTime, KeyboardState inputKey)
+        {
+            if (!inputKey.IsKeyDown(Keys.Up) && !inputKey.IsKeyDown(Keys.Down) && !inputKey.IsKeyDown(Keys.Left) && !inputKey.IsKeyDown(Keys.Right))
+            {
+                if (currSpeed.X > 0)
+                {
+                    currSpeed.X -= accSpeed.X;
+                    if (currSpeed.X < 0) currSpeed.X = 0;
+                }
+                else if (currSpeed.X < 0)
+                {
+                    currSpeed.X += accSpeed.X;
+                    if (currSpeed.X > 0) currSpeed.X = 0;
+                }
+
+                if (currSpeed.Y > 0)
+                {
+                    currSpeed.Y -= accSpeed.Y;
+                    if (currSpeed.Y < 0) currSpeed.Y = 0;
+                }
+                else if (currSpeed.Y < 0)
+                {
+                    currSpeed.Y += accSpeed.Y;
+                    if (currSpeed.Y > 0) currSpeed.Y = 0;
+                }
+            }
+            else
+            {
+                if (inputKey.IsKeyDown(Keys.Up))
+                {
+                    currSpeed.Y -= accSpeed.Y;
+                }
+                else if (inputKey.IsKeyDown(Keys.Down))
+                {
+                    currSpeed.Y += accSpeed.Y;
+                }
+
+                if (inputKey.IsKeyDown(Keys.Left))
+                {
+                    currSpeed.X -= accSpeed.X;
+                }
+                else if (inputKey.IsKeyDown(Keys.Right))
+                {
+                    currSpeed.X += accSpeed.X;
+                }
+            }
+            if (currSpeed.X > maxSpeed.X) currSpeed.X = maxSpeed.X;
+            if (currSpeed.Y > maxSpeed.Y) currSpeed.Y = maxSpeed.Y;
+            if (currSpeed.X < -maxSpeed.X) currSpeed.X = -maxSpeed.X;
+            if (currSpeed.Y < -maxSpeed.Y) currSpeed.Y = -maxSpeed.Y;
+
+            Vector2 potentialPosition = position;
+            potentialPosition.X += currSpeed.X;
+            potentialPosition.Y += currSpeed.Y;
+            collisionManager.HandleCollisions(this, potentialPosition, civs);
+
+            position.X += currSpeed.X;
+            position.Y += currSpeed.Y;
+
+            if (position.X > Game1.screenWidth - frameWidth)
+            {
+                position.X = Game1.screenWidth - frameWidth;
+                currSpeed.X = 0;
+            }
+            if (position.Y > Game1.screenHeight - frameHeight)
+            {
+                position.Y = Game1.screenHeight - frameHeight;
+                currSpeed.Y = 0;
+            }
+            if (position.X < 0)
+            {
+                position.X = 0;
+                currSpeed.X = 0;
+            }
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+                currSpeed.Y = 0;
+            }
+
+            if (inputKey.IsKeyDown(Keys.Up) && position.Y - currSpeed.Y >= 0)
+            {
+                //position.Y -= currSpeed.Y; // ga naar boven
+                currDirection = Direction.Up;
+                idleFrameIndex = 12; // idle frame bijhouden
+            }
+            else if (inputKey.IsKeyDown(Keys.Down) && position.Y + currSpeed.Y <= Game1.screenHeight - frameHeight)
+            {
+                //position.Y += currSpeed.Y; // ga naar beneden
+                currDirection = Direction.Down;
+                idleFrameIndex = 0; // idle frame bijhouden
+            }
+            else if (inputKey.IsKeyDown(Keys.Left) && position.X - currSpeed.X >= 0)
+            {
+                //position.X -= currSpeed.X; // ga naar links
+                currDirection = Direction.Left;
+                idleFrameIndex = 4; // idle frame bijhouden
+            }
+            else if (inputKey.IsKeyDown(Keys.Right) && position.X + currSpeed.X <= Game1.screenWidth - frameWidth)
+            {
+                //position.X += currSpeed.X; // ga naar rechts
+                currDirection = Direction.Right;
+                idleFrameIndex = 8; // idle frame bijhouden
+            }
+            else currDirection = Direction.Idle;
+            //currSpeed = walkSpeed; // terug op walkspeed zetten
+        }
+
+        public void Update(GameTime gametime)
+        {
+            base.Update(gametime);
+
+            KeyboardState inputKey = Keyboard.GetState(); // ophalen bewegingsinput
+
+            Move(gametime, inputKey); // move method uitvoeren afh vd keyboardstate
+
+            UpdateAnimationFrame(gametime); // animation update
+        }
+
+        private void UpdateAnimationFrame(GameTime gametime)
+        {
+            switch (currDirection) // animation frame update/correctie voor huidig direction
+            {
+                case Direction.Up:
+                    animation.currFrame = animation.frames[12 + animation.frames.IndexOf(animation.currFrame) % 4];
+                    break;
+                case Direction.Down:
+                    animation.currFrame = animation.frames[0 + animation.frames.IndexOf(animation.currFrame) % 4];
+                    break;
+                case Direction.Left:
+                    animation.currFrame = animation.frames[4 + animation.frames.IndexOf(animation.currFrame) % 4];
+                    break;
+                case Direction.Right:
+                    animation.currFrame = animation.frames[8 + animation.frames.IndexOf(animation.currFrame) % 4];
+                    break;
+                case Direction.Idle:
+                    animation.currFrame = animation.frames[idleFrameIndex];
+                    break;
+            }
+        }
+
+        public Vector2 GetCenter() // centrum coordinaten van de held
+        {
+            return new Vector2(position.X + frameWidth / 2f, position.Y + frameHeight / 2f);
+        }
+        public void LoseLive()
+        {
+            if (lives > 0) lives--;
+            else lives = 0;
+        }
+    }
+}
