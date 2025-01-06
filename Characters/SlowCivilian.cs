e491258935e8f51a448eeda45adb11965e86b135
Vislan <s144510@ap.be>
2025-01-06 21:01:10 +0100

    add slowcivilian (was unstaged in last commit)

---
+++ Characters/SlowCivilian.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentProject.Characters
+{
+    public class SlowCivilian : Civilian
+    {
+        public SlowCivilian(Texture2D texture, int fwidth, int fheight, Direction startDirection, int aiversion, Hero hero)
+            : base(texture, fwidth, fheight, startDirection, aiversion, hero)
+        {
+            walkSpeed = 2f;
+        }
+    }
+}
